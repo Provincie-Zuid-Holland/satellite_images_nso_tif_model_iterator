9d193fe0a31cdd1cc9ffadd747e4f49c922c41f1
Pieter Kouyzer <paj.kouyzer@pzh.nl>
2024-01-22 10:11:07 +0000

    Deal with variable number of bands

--- src/loaders/normalize_scaler_loader.py
+++ src/loaders/normalize_scaler_loader.py
@@ -1,3 +1,4 @@
+import glob
 import os
 
 from nso_ds_classes.nso_ds_normalize_scaler import scaler_class_all
@@ -15,18 +16,6 @@ class NormalizeScalerLoader:
 
     def load(self) -> scaler_class_all:
         tif_file_name = os.path.basename(self.tif_filepath)
-        file_bands = [
-            os.path.join(
-                self.path_to_scaler_files, tif_file_name + f"_band{band_id}.save"
-            )
-            for band_id in range(1, 7)
-        ]
+        band_filepaths = glob.glob(f"{self.path_to_scaler_files}/{tif_file_name}*")
 
-        return scaler_class_all(
-            scaler_file_band1=file_bands[0],
-            scaler_file_band2=file_bands[1],
-            scaler_file_band3=file_bands[2],
-            scaler_file_band4=file_bands[3],
-            scaler_file_band5=file_bands[4],
-            scaler_file_band6=file_bands[5],
-        )
+        return scaler_class_all(scaler_band_filepaths=band_filepaths)
