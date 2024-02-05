56394c23b89519517f618ee0e3d5c6f4d0ea86f1
MW-88 <m.r.dewinter88@live.nl>
2024-02-05 11:57:18 +0000

    keep coepelduynen iterators

--- coepelduynen_main_iterator.py
+++ coepelduynen_main_iterator.py
@@ -1,35 +1,60 @@
-from tif_model_iterator import tif_kernel_iterator
 import pickle
+
 import settings
+from src.filenames.file_name_generator import OutputFileNameGenerator
+from src.tif_model_iterator import tif_kernel_iterator
 import glob
-import pickle
 
-
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     filename = settings.MODEL_PATH
-    loaded_model = pickle.load(open(filename, 'rb'))
+    print(filename)
+    loaded_model = pickle.load(open(filename, "rb"))
 
+    tif_files = [
+        file
+        for file in glob.glob("E:/data/coepelduynen/2023*PNEO*re_ndvi*asphalt*.tif")
+    ]
 
-    tif_files = [file for file in glob.glob("E:/data/coepelduynen/2023*PNEO*re_ndvi*asphalt*.tif")]
-
-
-    for tif_file in tif_files[0:1]:
-        print("-----")
+    for tif_file in tif_files:
+        print("----------")
         print(tif_file)
 
-        tif_file = tif_file.replace("\\","/")
-        output_location = "E:/output/Coepelduynen_segmentations/"+tif_file.split("/")[-1].replace(".tif", ".shp").replace("\\","/")
-        date = output_location.split("/")[-1].split("_")[0]
+        tif_file = tif_file.replace("\\", "/")
+        output_path = "E:/output/Coepelduynen_segmentations_production/"
+        date = output_path.split("/")[-1].split("_")[0]
 
-        nso_tif_kernel_iterator_generator = tif_kernel_iterator.tif_kernel_iterator_generator(tif_file)
+        output_file_name = (
+            "E:/output/Coepelduynen_segmentations_production/"
+            + tif_file.split("/")[-1].replace(".tif", ".shp").replace("\\", "/")
+        )
 
-        #print(filename)
-        #print(filename.split("/")[-1].split["_"])
-        #for file in glob.glob("C:/repos/satellite-images-nso-datascience/scalers/ "):
+        scaler = pickle.load(
+            open(
+                [
+                    file
+                    for file in glob.glob(
+                        "C:/repos/satellite-images-nso-datascience/scalers/"
+                        + date
+                        + "*StandardScaler.pkl"
+                    )
+                ][0],
+                "rb",
+            )
+        )
 
-        ascaler = pickle.load(open([file for file in glob.glob("C:/repos/satellite-images-nso-datascience/scalers/"+date+"*.pkl")][0] ,'rb'))
-     
+        output_file_name_generator = OutputFileNameGenerator(
+            output_path=output_path, output_file_name=output_file_name
+        )
 
-        #nso_tif_kernel_iterator_generator.predict_all_output(loaded_model, output_location, parts=10, multiprocessing = True, normalize_scaler="PATH/TO/SCALER" )
-        nso_tif_kernel_iterator_generator.predict_all_output(loaded_model, output_location, parts=7, multiprocessing = True, normalize_scaler= ascaler , bands = ascaler.columns_names, aggregate_output = False )
+        nso_tif_kernel_iterator_generator = (
+            tif_kernel_iterator.TifKernelIteratorGenerator(
+                path_to_tif_file=tif_file,
+                model=loaded_model,
+                output_file_name_generator=output_file_name_generator,
+                parts=4,
+                normalize_scaler=scaler,
+                aggregate_output=2,
+            )
+        )
+
+        nso_tif_kernel_iterator_generator.predict_all_output()
