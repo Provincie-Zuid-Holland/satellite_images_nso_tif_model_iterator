69764bc31b855cc411cd576b6545d73353e59b42
MW-88 <m.r.dewinter88@live.nl>
2024-08-06 13:47:39 +0000

    Divided functions for more clearity

--- src/satellite_images_nso_tif_model_iterator/h3_hexagons/nso_tif_model_iterater_hexagon_output.py
+++ src/satellite_images_nso_tif_model_iterator/h3_hexagons/nso_tif_model_iterater_hexagon_output.py
@@ -6,22 +6,24 @@ from shapely.geometry import Polygon
 import timer
 
 
-def output_h3_hexagons_from_pixels(path_to_file, resolution=12, crs="28892"):
+def read_data(path_to_file, crs):
     """
-    Makes single geo points output with label output into a hexagon output.
 
-    @param path_to_file: path to a file which contains row pixel based geopoint data.
-    @param resolution: the resolution of the hexagon, look at https://h3geo.org/docs/core-library/restable/ for details
+    Function for reading in the geopoint .csv or .parquet file, although .parquet is recommended.
+
+    TODO: For now it assumes the crs has to be in 4326, assuming that the output date is alwaus in 28892, this has to be more dynamic.
+
+    @param path_to_file: path to where the file is located.
+    @param crs: the coordinate reference system for the geopoint, for now only 28892 whill be converted to 4326
     """
 
     # TODO: Make clear exceptions to check if the data is valid.
-    # TODO: Not make a assumption about the base crs!
     if crs is "28892":
+        # Make a transformer to convert from 28992 to 4326
         transformer = pyproj.Transformer.from_crs(
             "EPSG:28992", "EPSG:4326", always_xy=True
         )
 
-    # final_output_path = self.output_file_name_generator.generate_final_output_path()
     df = []
 
     if ".csv" in path_to_file:
@@ -46,6 +48,11 @@ def output_h3_hexagons_from_pixels(path_to_file, resolution=12, crs="28892"):
     else:
         df["lon"], df["lat"] = df["rd_x"], df["rd_y"]
 
+    return df
+
+
+def transform_data(df, resolution):
+
     start = timer()
     # match the point with a hexagon id
     df["hexagon_id"] = df.apply(
@@ -64,10 +71,23 @@ def output_h3_hexagons_from_pixels(path_to_file, resolution=12, crs="28892"):
         lambda x: Polygon(h3.h3_to_geo_boundary(x["hexagon_id"])), axis=1
     )
 
+    print("Hexagon indexing finished in: " + str(timer() - start) + " second(s)")
+    return df
+
+
+def output_h3_hexagons_from_pixels(path_to_file, resolution=12, crs="28892"):
+    """
+    Makes single geo points output with label output into a hexagon output.
+
+    @param path_to_file: path to a file which contains row pixel based geopoint data.
+    @param resolution: the resolution of the hexagon, look at https://h3geo.org/docs/core-library/restable/ for details
+    """
+
+    df = read_data(path_to_file, crs)
+    df = transform_data(df, resolution)
+
     # Export results to a .geojson
     gpd.GeoDataFrame(df, geometry=df["geometry"]).to_file(
         path_to_file.split(".")[0] + "_hexagons_res" + str(resolution) + ".geojson",
         driver="GeoJSON",
     )
-
-    print("Hexagon indexing finished in: " + str(timer() - start) + " second(s)")
